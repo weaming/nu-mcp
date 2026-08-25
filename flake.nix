e09f658f595f00bd18167b95b2f9f210ff334c3c
Muhammad Ridwan <25993873+duanxyz@users.noreply.github.com>
2026-08-26 06:49:56 +0800

    fix: use system-specific default package in overlay (#130)

--- flake.nix
+++ flake.nix
@@ -195,7 +195,7 @@
 
       flake = {
         overlays.default = final: prev: {
-          nu-mcp = self.packages.default;
+          nu-mcp = self.packages.${final.system}.default;
         };
       };
     };
